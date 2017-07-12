85d8b76576a3c9c5a810faf3274ad440c72e51d4
alok87 <alok.singh@practo.com>
2017-07-12 08:16:14 +0530

    Insert in sorted arr

--- gfg/ds/array/2-insert.go
+++ gfg/ds/array/2-insert.go
@@ -3,26 +3,25 @@ package main
 import (
     "fmt"
     "math/rand"
-    "sort"
-    "github.com/alok87/goutils/pkg/random"
-    "github.com/alok87/goutils/pkg/unique"
 )
 
-func insert(arr []int, elem int) ([]int, bool) {
-    
-
+func insert(arr [5]int, elem int) ([5]int, bool) {
+    i := len(arr) - 2
+    for ;(i>=0 && arr[i] > elem); i-- {
+        arr[i+1] = arr[i] 
+    }
+    arr[i+1] = elem
+    return arr, true
 }
 
 func main() {
     min := 1
     max := 20
-    n := 10
-    arr := random.RangeInt(min, max, n)
-    uniqueArr := unique.Ints(arr)
-    sort.Ints(uniqueArr)
-    n := rand.Intn(max) + min
-    fmt.Println("Inserting:", n)
-    fmt.Println("Array=", uniqueArr)
-    newArr, isInserted := insert(uniqueArr, noSearch)
-    fmt.Println("Array=", newArr, isInserted) 
+    newArr := [5]int{1, 2, 3, 5}
+    number := rand.Intn(max) + min
+    number = 0 
+    fmt.Println("Inserting:", number)
+    fmt.Println("Array=", newArr)
+    arr2, isInserted := insert(newArr, number)
+    fmt.Println("Array=", arr2, isInserted) 
 }
